253ae72a7c6738118699fa339dac1f3c5cb4d02f
dny343 <dnynshwrshinde007@gmail.com>
2024-08-21 12:35:21 +0530

    frontend

---
+++ dist/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from './context/StoreContext.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+  </BrowserRouter>
+)
