4f84637b4597352bf80333e4be18dab8dcbcfb17
dny343 <dnynshwrshinde007@gmail.com>
2024-08-21 13:02:08 +0530

    frontend

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  base: 'https://dny343.github.io/restaurant-food-delivery-app/',
-}
+export default defineConfig({
+  plugins: [react()],
+  base: 'https://dny343.github.io/restaurant-food-delivery-app/'
+});
