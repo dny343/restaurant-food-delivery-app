acc56324b909f32ca58d95dc6acd130e33644338
dny343 <dnynshwrshinde007@gmail.com>
2024-08-21 12:24:01 +0530

    frontend

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
